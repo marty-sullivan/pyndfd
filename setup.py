42107cda7cbca012d48f9681746aaefd3faee933
Marty J Sullivan <marty.sullivan@cornell.edu>
2014-12-10 16:25:50 -0500

    reorganize for pyndfd

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyndfd',
+  packages = ['pyndfd'],
+  version = '0.1',
+  license = 'MIT License',
+  description = 'Python routines for easy caching/retrieval of NWS\'s NDFD variables',
+  author = 'Marty J. Sullivan',
+  author_email = 'marty.sullivan@cornell.edu',
+  url = '
+)
