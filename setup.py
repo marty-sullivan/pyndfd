e87b548d129d8ec4c9614e41e48ed66df082139c
Marty J Sullivan <marty.sullivan@cornell.edu>
2015-06-30 14:12:46 -0400

    Stage v0.6

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'pyndfd',
   packages = ['pyndfd'],
-  version = '0.4',
+  version = '0.6',
   license = 'MIT License',
   description = 'Python routines for easy caching/retrieval of NWS\'s NDFD variables',
   author = 'Marty J. Sullivan',
   author_email = 'marty.sullivan@cornell.edu',
   url = 'https://github.com/marty-sullivan/pyndfd',
-  download_url = 'https://github.com/marty-sullivan/pyndfd/tarball/0.4',
+  download_url = 'https://github.com/marty-sullivan/pyndfd/tarball/0.6',
   install_requires = ['numpy', 'pygrib', 'pyproj'],
   keywords = ['noaa', 'ndfd', 'nws', 'weather', 'forecast', 'cornell', 'amospheric', 'science']
 )
